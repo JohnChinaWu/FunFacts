c3449430aecd90f067c30fd04ba07ddc91e5ddc1
wuwenjun <johnwu1016@gmail.com>
2017-09-13 18:29:56 +0800

    Access Views in Code: Declare variables

--- app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
@@ -2,8 +2,13 @@ package com.johnwu1016.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

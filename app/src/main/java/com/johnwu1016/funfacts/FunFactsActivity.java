cde861c67867dac6f545a287bac838e9c06cf516
wuwenjun <johnwu1016@gmail.com>
2017-09-13 19:32:09 +0800

    dynamically change the background color

--- app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.johnwu1016.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,6 +15,7 @@ public class FunFactsActivity extends AppCompatActivity {
     // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assign the Views from the layout file to the corresponding variables
         factTextView = (TextView) findViewById(R.id.factsTextView);
         showFactButton = (Button) findViewById(R.id.showFactsButton);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -30,6 +34,7 @@ public class FunFactsActivity extends AppCompatActivity {
 
                 // Update the screen with new fact
                 factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(Color.RED);
             }
         };
         showFactButton.setOnClickListener(listener);

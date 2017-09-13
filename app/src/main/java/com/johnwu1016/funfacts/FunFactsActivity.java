4c012d58a849956723a3ae1cb7d3d0870d1b4044
wuwenjun <johnwu1016@gmail.com>
2017-09-13 20:08:20 +0800

    the Android log

--- app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
@@ -3,6 +3,7 @@ package com.johnwu1016.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare our View variables
@@ -42,6 +44,7 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
-        Toast.makeText(this, "Yap! Our Activity was created!", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Yap! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }

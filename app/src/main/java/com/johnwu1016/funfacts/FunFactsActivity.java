ca1c4116a372f92d5a5890e6cd69d54e20d714ef
wuwenjun <johnwu1016@gmail.com>
2017-09-13 18:41:37 +0800

    make a button do something

--- app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/johnwu1016/funfacts/FunFactsActivity.java
@@ -2,6 +2,7 @@ package com.johnwu1016.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +18,14 @@ public class FunFactsActivity extends AppCompatActivity {
 
         // Assign the Views from the layout file to the corresponding variables
         factTextView = (TextView) findViewById(R.id.factsTextView);
+        showFactButton = (Button) findViewById(R.id.showFactsButton);
 
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
